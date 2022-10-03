58c81f38f02dab8957f2922000a0fda4f3270038
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-10-02 21:47:22 -0400

    add user routes

--- server.js
+++ server.js
@@ -12,4 +12,20 @@ app.get("/", (req, res) => {
     res.render("Index");
 });
 
+app.get("/users", (req, res) => {
+    res.render("users/Index");
+})
+
+app.get("/users/new", (req, res) => {
+    res.render("users/New");
+})
+
+app.get("/users/:id", (req, res) => {
+    res.render("users/Show");
+})
+
+app.get("/users/:id/edit", (req, res) => {
+    res.render("users/Edit");
+})
+
 app.listen("3000", () => {console.log("Server is running on port 3000");});
