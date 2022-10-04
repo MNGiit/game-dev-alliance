a0cbe5691132cef75f86caf91ff7bf5073f8ccf3
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-10-03 22:46:04 -0400

    add login page

---
+++ views/users/Login.jsx
@@ -0,0 +1,28 @@
+const React = require("react");
+// const DefaultLayout = require("./layouts/Default");
+
+class Login extends React.Component {
+    render() {
+        // Put props obj to destructure //
+        // const { exampleOfSomething } = this.props; // also try const exampleOfSomething = this.props.exampleOfSomething
+
+        return (
+            // <DefaultLayout title={"Title"}>
+            // Could use fieldset
+            <>
+                <h1>Log in</h1>
+                <form action="/users/new" method="POST">
+                    <label>
+                        Username: <input type="text" name="username" required />
+                    </label> <br />
+                    <label>
+                        Password: <input type="password" name="password" required />
+                    </label> <br />
+                    <input type="submit" name="" value="Log in"/>
+                </form>
+            </>
+        );
+    }
+}
+
+module.exports = Login;
